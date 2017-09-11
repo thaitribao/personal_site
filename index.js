fc983d0b60c27f9247bbbd7dbfbf91bcd8e02765
Bao Thai <baothai120708@gmail.com>
2017-09-11 19:24:18 -0400

    Installed ejs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express'); 
 var app = express(); 
+var ejs = require('ejs'); 
 
+//Home page
 app.get('/', function(req, res){
     res.send('Hello Word from Port 8000!');
 }); 
+
 
 app.listen(8000, function(){
     console.log('Example app listening on port 8000!'); 
