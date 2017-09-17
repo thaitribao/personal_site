65ce80943c286c5efa099fcbbef5a11d56a9d4b2
Bao Thai <baothai120708@gmail.com>
2017-09-16 22:51:49 -0400

    Add projects and blogs path. Use ejs to render templates. Use partials for header and footer.

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var express = require('express'); 
-var app = express(); 
-var ejs = require('ejs'); 
+var app = express();  
 
-//Home page
+app.set('view engine', 'ejs');
+
+//Home page - resume-like page will be hosted here
 app.get('/', function(req, res){
-    res.send('Hello Word from Port 8000!');
+    res.render("home");
 }); 
 
+//Projects - page containing links to several projects
+app.get('/projects', function(req, res){
+    res.render('projects'); 
+});
+//Blogs - page containing links to several blogs
+app.get('/blogs', function(req, res){
+    res.render('blogs');  
+});
+//Blogs - cycling
+app.get('/blogs/bikes', function(req, res){
+    res.send('Reached the bike blogs'); 
+});
+//Blogs - technical 
+app.get('/blogs/technical', function(req, res){
+    res.send('Reached the technical blogs');
+});
+//Blogs - food
+app.get('/blogs/food', function(req, res){
+    res.send('Reached the food blogs'); 
+});
 
 app.listen(8000, function(){
     console.log('Example app listening on port 8000!'); 
